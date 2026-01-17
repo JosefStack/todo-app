7935125f53e31db964931bae2b63ae2cfbc44c64
JosefStack <jojohnson350@gmail.com>
2026-01-17 20:42:55 +0200

    basic functions test

--- main.js
+++ main.js
@@ -5,10 +5,32 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+
 app.use(bodyParser.urlencoded( {extended : true}) );
 app.use(express.static("public"));
 
 
+let tasksList = [];
+
+
+app.post("/add", (req, res) => {
+    tasksList.push(
+        {
+            task: req.body.task
+        }
+    )
+    res.redirect("/");
+    
+})
+
+
+app.get("/", (req, res) => {
+    res.render("index.ejs", 
+        {
+            tasks : tasksList
+        }
+    );
+})
 
 
 app.listen(port, function() {
