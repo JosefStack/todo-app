3440f4362bcb9fccd98f923c5f4ad2c04e5ff09d
JosefStack <jojohnson350@gmail.com>
2026-01-18 14:19:39 +0200

    masonry layout base

--- public/scripts/masonry.js
+++ public/scripts/masonry.js
@@ -1,2 +1,19 @@
 const container = document.querySelector(".tasks-container");
-const tasks = document.querySelectorAll("task-item");
+const tasks = document.querySelectorAll(".task-item");
+
+
+const resize = function() {
+    tasks.forEach((task) => {
+    const height = task.firstElementChild.clientHeight; 
+    console.log(height);
+    const span = Math.ceil(height/(10+10));
+    console.log(span);
+    task.style.gridRowEnd = `span ${span}`;
+})  
+}
+
+resize();
+window.addEventListener("resize", resize)
+
+
+
